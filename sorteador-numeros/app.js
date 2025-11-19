2c679083fd21b5c3ab754f79981f188b4b488453
Tales Bruno Santos Jesus <tales.sbruno@totvs.com.br>
2025-11-19 08:27:07 -0300

    Acrescentei funcionalidades de alterar status do botão e alterar a messagem quando o valores sorteados aparecerem

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,20 +1,52 @@
+
 function sortear() {
     let quantidadeDeNumero = parseInt(document.getElementById('quantidade').value);
     let numeroDe = parseInt(document.getElementById('de').value);
     let numeroAte = parseInt(document.getElementById('ate').value);
 
+
     let sorteados = [];
     let numero;
-    
 
-    for( let i = 0; i < quantidadeDeNumero; i++){
-        numero = numeroAleatorio(numeroDe,numeroAte);
+
+    for (let i = 0; i < quantidadeDeNumero; i++) {
+        numero = numeroAleatorio(numeroDe, numeroAte);
+
+        while(sorteados.includes(numero)){
+            numero = numeroAleatorio(numeroDe ,numeroAte);
+        }
         sorteados.push(numero);
     };
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
+    
+    alteraStatusBotao();
+
 };
 
-function numeroAleatorio(numeroDe, numeroAte){
-    return Math.floor(Math.random() * (numeroAte  - numeroDe + 1)) + numeroDe;
+
+function numeroAleatorio(numeroDe, numeroAte) {
+    return Math.floor(Math.random() * (numeroAte - numeroDe + 1)) + numeroDe;
 };
+
+function alteraStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+
+    alteraStatusBotao();
+}
